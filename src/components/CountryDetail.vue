8ccda38dcc1fa8c0bb766dff65d172103f5cf7f9
user <you@exampalex.com>
2019-10-23 14:33:40 +0100

    first commit

---
+++ src/components/CountryDetail.vue
@@ -0,0 +1,17 @@
+<template lang="html">
+  <div v-if="country">
+    <h3> {{ country.name }}</h3>
+    <h4> {{ country.capital }}</h4>
+    <h4> {{ country.population }} </h4>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "country-detail",
+  props: ["country"]
+}
+</script>
+
+<style lang="css" scoped>
+</style>
