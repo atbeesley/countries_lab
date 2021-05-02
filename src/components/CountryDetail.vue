deaee561e272cdd49ada0151bd697c8a14e50dbd
atbeesley <alexander.beesley@gmail.com>
2021-05-02 09:26:40 +0100

    demonym v-if

--- src/components/CountryDetail.vue
+++ src/components/CountryDetail.vue
@@ -3,7 +3,7 @@
     <h3> {{ country.name }}</h3>
     <h4> {{ country.capital }}</h4>
     <h4> {{ country.population }} </h4>
-    <h4>The inhabitants are known as {{country.demonym}}s.</h4>  
+    <h4 v-if="country.demonym">The inhabitants are known as {{country.demonym}}s.</h4>  
     <img :src="`${country.flag}`"></v-img> 
   </div>
 </template>
