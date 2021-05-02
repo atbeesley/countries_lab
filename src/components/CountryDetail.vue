44a827c97dce21c2d5a47f5d62d28f9d318e8a4e
atbeesley <alexander.beesley@gmail.com>
2021-05-02 10:26:27 +0100

    region working

--- src/components/CountryDetail.vue
+++ src/components/CountryDetail.vue
@@ -1,8 +1,9 @@
 <template lang="html">
   <div v-if="country" class="detailbox">
     <h3> {{ country.name }}</h3>
-    <h4> {{ country.capital }}</h4>
-    <h4> {{ country.population }} </h4>
+    <h4> Capital: {{ country.capital }}</h4>
+    <h4> Population: {{ country.population }} </h4>
+    <h4>It is located somewhere deep in the {{country.region}} region.</h4>
     <h4 v-if="country.demonym">The inhabitants are known as {{country.demonym}}s.</h4>  
     <img 
     :src="`${country.flag}`"   
