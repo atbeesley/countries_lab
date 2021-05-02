a6c829d9171d55cc1c1fccbc068b2e65a3ef8b87
atbeesley <alexander.beesley@gmail.com>
2021-05-02 09:24:54 +0100

    flag working

--- src/components/CountryDetail.vue
+++ src/components/CountryDetail.vue
@@ -3,7 +3,8 @@
     <h3> {{ country.name }}</h3>
     <h4> {{ country.capital }}</h4>
     <h4> {{ country.population }} </h4>
-    <h4>The inhabitants are known as {{country.demonym}}s.</h4>   
+    <h4>The inhabitants are known as {{country.demonym}}s.</h4>  
+    <img :src="`${country.flag}`"></v-img> 
   </div>
 </template>
 
@@ -12,6 +13,7 @@ export default {
   name: "country-detail",
   props: ["country"]
 }
+
 </script>
 
 <style lang="css" scoped>
