2802783eefd13763f2883442fbfe22e05d62af1c
atbeesley <alexander.beesley@gmail.com>
2021-05-03 11:36:59 +0100

    languages working

--- src/components/CountryDetail.vue
+++ src/components/CountryDetail.vue
@@ -1,11 +1,14 @@
 <template lang="html">
   <div v-if="country" class="detailbox">
-    <h3>{{ country.name }}</h3>
+    <h2>{{ country.name }}</h2>
     <h4 v-if="country.capital"> Capital: {{ country.capital }}</h4>
     <h4>Population: {{ country.population }} </h4>
-    <h4 v-if="country.region">It is located somewhere deep in the {{country.region}} region.</h4>
+    <h3>Some tasty factoids about {{ country.name }}:</h3>
+    <h4 v-if="country.region">It is located somewhere in the {{country.region}} region.</h4>
     <h4 v-if="country.demonym">The inhabitants are known as {{country.demonym}}s.</h4>  
-    <!-- <h4>The locals speak {{country.languages[2].name}}.</h4> -->
+    <h4 v-if="country.languages">The locals parley in {{country.languages[0].name}}.</h4>
+    <!-- <h4 v-if="country.regionalBlocs[0]">It belongs to the {{country.regionalBlocs[0].name}}.</h4> -->
+    <!-- <h4 v-if="country.regionalBlocs.size()>1">It is part of {{country.regionalBlocs[1]}}.</h4> -->
     <img 
     :src="`${country.flag}`"   
     :alt="`Flag of ${country.name}`"
