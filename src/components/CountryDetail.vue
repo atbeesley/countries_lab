334d0ba3fcf41393981dfdf9a0e6bdeb874f5ceb
atbeesley <alexander.beesley@gmail.com>
2021-05-03 11:57:22 +0100

    formatting changes (ul & p-ing)

--- src/components/CountryDetail.vue
+++ src/components/CountryDetail.vue
@@ -1,14 +1,15 @@
 <template lang="html">
   <div v-if="country" class="detailbox">
     <h2>{{ country.name }}</h2>
-    <h4 v-if="country.capital"> Capital: {{ country.capital }}</h4>
-    <h4>Population: {{ country.population }} </h4>
+    <p v-if="country.capital"> Capital: {{ country.capital }}</p>
+    <p>Population: {{ country.population }} </p>
     <h3>Some tasty factoids about {{ country.name }}:</h3>
-    <h4 v-if="country.region">It is located somewhere in the {{country.region}} region.</h4>
-    <h4 v-if="country.demonym">The inhabitants are known as {{country.demonym}}s.</h4>  
-    <h4 v-if="country.languages">The locals parley in {{country.languages[0].name}}.</h4>
-    <!-- <h4 v-if="country.regionalBlocs[0]">It belongs to the {{country.regionalBlocs[0].name}}.</h4> -->
-    <!-- <h4 v-if="country.regionalBlocs.size()>1">It is part of {{country.regionalBlocs[1]}}.</h4> -->
+    <ul>
+    <li v-if="country.region"><p v-if="country.region">It is located somewhere in the {{country.region}} region.</p></li>
+    <li v-if="country.demonym"><p v-if="country.demonym">The inhabitants are known as {{country.demonym}}s.</p></li>
+    <li  v-if="country.languages"><p v-if="country.languages">The locals parley in {{country.languages[0].name}}.</p></li>
+    <!-- </li><p v-if="typeof country.regionalBlocs !== undefined">It belongs to the {{country.regionalBlocs[0].name}}.</p></li> -->
+    </ul>
     <img 
     :src="`${country.flag}`"   
     :alt="`Flag of ${country.name}`"
